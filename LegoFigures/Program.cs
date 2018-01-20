5daed17ca14d248b89a58e28f06202b56b5b7ebf
Ben Harrington <harringtonben@icloud.com>
2018-01-20 13:26:29 -0600

    finished establishing class for characters

---
+++ LegoFigures/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var LegoCharacters = new List<figure>
+        }
+    }
+}
---
+++ LegoFigures/figure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoFigures
+{
+    class figure
+    {
+        public string Hair { get; set; }
+        public string Head { get; set; }
+        public string Torso { get; set; }
+        public string Legs { get; set; }
+        public string Accessories { get; set; }
+        public bool IsAFighter { get; set; }
+    }
+}
